c34206e0381a029ea7ef1b55859e8b6a7735ecd3
Anton <panteleev23@gmail.com>
2021-03-07 22:50:27 +1100

    ARCH: modal windows added. game back-to-menu btn added with js script

--- script.js
+++ script.js
@@ -1,67 +1,30 @@
 'use strict';
 
-// const secretNumber = Math.trunc(Math.random() * 20 + 1);
-let secretNumber = 10
-let score = 20;
-let highscore = 0;
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-const message = (message) => {
-  document.querySelector('.message').textContent = message
-}
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-document.querySelector('.check').addEventListener('click', function () {
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-  const guess = Number(document.querySelector('.guess').value)
-  console.log(guess, typeof guess);
-  console.log('secretnumber: ' + secretNumber, typeof secretNumber);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-  // WIN OR ========
-  // ==========
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
 
-  // === evaluation LOGIC ====
-  if (guess === secretNumber && score > 0) {
-    document.querySelector('.number').textContent = secretNumber
-    message('Congrats! Yeeew 🎆')
-    document.querySelector('body').style.backgroundColor = '#68830a'
-    highscore = score
-  } else if (score < 1) { //FAIL
-    document.querySelector('.number').textContent = secretNumber
-    message('You lost🏳️')
-    score = 0
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
   }
-
-  if (!guess) {
-    message('please type a positive number🚸')
-  } else if (guess < secretNumber) {
-    message('go up 📈')
-    score--
-  } else if (guess > secretNumber) {
-    message('go down ↘️')
-    score--
-  }
-
-
-
-  document.querySelector('.score').textContent = score
-  document.querySelector('.highscore').textContent = highscore
-
-})
-
-// document.querySelector('.number').textContent = number
-
-
-document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  // secretNumber = 2
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-  // document.querySelector('.message').textContent = 'Start guessing...';
-  message('Start guessing...');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
-
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
 });
